6602de20616105c97ae928e2d11df17329f5e46c
sarusikisslaszlo <s.laci0101@gmail.com>
2021-04-22 17:00:47 +0200

    login layout

--- app/src/main/java/com/example/healthcareservice/MainActivity.java
+++ app/src/main/java/com/example/healthcareservice/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.healthcareservice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void login(View view) {
+        EditText username = findViewById(R.id.editTextUserName);
+        EditText password = findViewById(R.id.editTextPassword);
+
+        String userNameString = username.getText().toString();
+        String passwordString = password.getText().toString();
+
+        Log.i(LOG_TAG, "Bejelentkezett: " + userNameString + ", jelszó: " + passwordString);
     }
 }
